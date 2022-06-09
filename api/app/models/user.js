db319ab7d57653b4dfa1a3573cd4150f1701008a
Léo Wolff <wolf.pro@outlook.fr>
2022-06-09 20:57:35 +0200

    correction mineur route user -> member remplacé par users dans les requêtes SQL

--- api/app/models/user.js
+++ api/app/models/user.js
@@ -6,12 +6,12 @@ module.exports = {
     // La récupération de toutes les catégories ne justifie d'aucune manière la création d'une
     // fonction. Que ce soit au niveau de la simplification d'ecriture ou à l'optimisation par
     // cache de la récupération des infos
-    const result = await client.query('SELECT * FROM member');
+    const result = await client.query('SELECT * FROM users');
     return result.rows;
   },
 
   async findByPk(userId) {
-    const result = await client.query('SELECT * FROM member WHERE id = $1', [userId]);
+    const result = await client.query('SELECT * FROM users WHERE id = $1', [userId]);
 
     return result.rows[0];
   },
