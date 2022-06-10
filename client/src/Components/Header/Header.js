0d7f5cf58597c3c89d764af21ad1ab9bfb599e5f
LaurentGourouvin <gourouvin.laurent@gmail.com>
2022-06-10 15:33:58 +0200

    ajout du menu Burger pour le responsive(à compléter avec les différents liens)

--- client/src/Components/Header/Header.js
+++ client/src/Components/Header/Header.js
@@ -7,18 +7,24 @@ function Header({ setIsLogged, isLogged }) {
     return (
       <div className="Header">
         <h1 className="Header--website-title">Go For Walk</h1>
-        <Link to="/register" className="Header--a">Inscription</Link>
-        <Link to="/login" className="Header--a">Connexion</Link>
-        <Link to="/about" className="Header--a">A propos</Link>
+        <div className="Header--burger-menu space-y-2">
+          <div className="w-8 h-0.5 bg-white" />
+          <div className="w-8 h-0.5 bg-white" />
+          <div className="w-8 h-0.5 bg-white" />
+        </div>
+        <Link to="/register" className="Header--a ease-linear transform hover:scale-110 transition duration-150 px-6 inline">Inscription</Link>
+        <Link to="/login" className="Header--a ease-linear transform hover:scale-110 transition duration-150 px-6 inline">Connexion</Link>
+        <Link to="/about" className="Header--a ease-linear transform hover:scale-110 transition duration-150 px-6 inline">A propos</Link>
       </div>
     );
   }
   return (
     <div className="Header">
       <h1 className="Header--website-title">Go For Walk</h1>
-      <Link to="/" className="Header--a" onClick={() => setIsLogged(!isLogged)}> Deconnexion</Link>
-      <Link to="/profil" className="Header--a">Mon profil</Link>
-      <Link to="/about" className="Header--a">A propos</Link>
+      <h1 className="Header--website-burger">Burger</h1>
+      <Link to="/" className="Header--a ease-linear transform hover:scale-110 transition duration-150 px-6 inline" onClick={() => setIsLogged(!isLogged)}> Deconnexion</Link>
+      <Link to="/profil" className="Header--a ease-linear transform hover:scale-110 transition duration-150 px-6 inline">Mon profil</Link>
+      <Link to="/about" className="Header--a ease-linear transform hover:scale-110 transition duration-150 px-6 inline">A propos</Link>
     </div>
   );
 }
