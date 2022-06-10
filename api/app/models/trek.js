2f2cbd4f50ba1e28849a6ec2c1c10ff6bd443fb9
Léo Wolff <wolf.pro@outlook.fr>
2022-06-10 15:32:33 +0200

    modif requete sql trek par treks

--- api/app/models/trek.js
+++ api/app/models/trek.js
@@ -5,24 +5,24 @@ module.exports = {
     // La récupération de toutes les catégories ne justifie d'aucune manière la création d'une
     // fonction. Que ce soit au niveau de la simplification d'ecriture ou à l'optimisation par
     // cache de la récupération des infos
-    const result = await client.query('SELECT * FROM trek');
+    const result = await client.query('SELECT * FROM treks');
     return result.rows;
   },
 
   async findByPk(trekId) {
-    const result = await client.query('SELECT * FROM trek WHERE id = $1', [trekId]);
+    const result = await client.query('SELECT * FROM treks WHERE id = $1', [trekId]);
     return result.rows[0];
   },
 
   async update(trekId, trekData) {
     const fields = Object.keys(trekData).map((trek, index) => `"${trek}" = $${index + 1}`);
     const values = Object.values(trekData);
-    const result = await client.query(`UPDATE trek SET ${fields} WHERE id = ${trekId}`, values);
+    const result = await client.query(`UPDATE treks SET ${fields} WHERE id = ${trekId}`, values);
     return result.rows[0];
   },
 
   async delet(id) {
-    const result = await client.query('DELETE FROM trek WHERE id = $1', [id]);
+    const result = await client.query('DELETE FROM treks WHERE id = $1', [id]);
     return result.rows[0];
   },
 
@@ -33,7 +33,7 @@ module.exports = {
       fields.push(`"${key}"`);
       values.push(`$${values.length + 1}`);
     });
-    const result = await client.query(`INSERT INTO trek (${fields}) VALUES (${values}) RETURNING *`, Object.values(trekData));
+    const result = await client.query(`INSERT INTO treks (${fields}) VALUES (${values}) RETURNING *`, Object.values(trekData));
     return result.rows[0];
   },
 };
