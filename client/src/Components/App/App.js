5cb3841060c2d049179c6f177e5cf3633f106de5
LaurentGourouvin <gourouvin.laurent@gmail.com>
2022-06-13 14:13:50 +0200

    Ajout d'une route pour la page profil

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -5,6 +5,7 @@ import Header from '../Header/Header';
 import LoginForm from '../LoginForm/LoginForm';
 import SearchBar from '../SearchBar/SearchBar';
 import RegisterForm from '../RegisterForm/RegisterForm';
+import Profil from '../Profil/Profil';
 
 function App() {
   const [isLogged, setIsLogged] = useState(false);
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<SearchBar />} />
           <Route path="/login" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
+          <Route path="/profil" element={<Profil />} />
         </Routes>
       </div>
     </div>
