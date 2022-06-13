3693a33b4240bc95227dd8564c56e7ba0d7292cc
FranckBisschop <bisschopfranck@gmail.com>
2022-06-13 15:02:03 +0200

    correction mineur dans le schema de validation

--- api/app/routers/api/auth.js
+++ api/app/routers/api/auth.js
@@ -9,14 +9,32 @@ const authController = require('../../controllers/api/auth');
 const router = express.Router();
 router
   .route('/register')
-
+/**
+ * A User is create with the following parameters :
+     * @typedef {object} userTest
+     * @property {string} firstname - user firstname
+     * @property {string} name - user lastname
+     * @property {string} email - user email
+     * @property {string} password - user password
+ */
 /**
      * POST /api/auth/register
      * @summary Create New User
      * @tags Auth
-     * @param {User} request.body.required - user info
+     * @param {userTest} request.body.required - user info
      * @returns {object} 200 - Utilisateur crée
      */
   .post(validate('body', createSchema), controllerHandler(authController.register));
+
+router
+  .route('/login')
+  /**
+   * POST /api/auth/login
+   * @summary Login The User
+   * @tags Auth
+   * @param {string} email - user email
+   * @param {string} password - user password
+   * */
+  .post(controllerHandler(authController.login));
 
 module.exports = router;
