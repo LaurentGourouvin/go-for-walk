d2eae711b25d7b1f22281f0da23a02ade59b5ab7
LaurentGourouvin <gourouvin.laurent@gmail.com>
2022-06-14 16:52:05 +0200

    ajout des datas Treks pour travailler en local sans l'API

---
+++ client/src/dataStatic/data_treks.js
@@ -0,0 +1,67 @@
+const data = [
+
+  {
+    id: 2,
+    titre: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas doloremque autem porro provident, nesciunt nisi optio temporibus, molestiae inventore aliquid fugit ab voluptas, ad iure sapiente quae laudantium impedit suscipit architecto assumenda hic nihil exercitationem ipsa! Soluta qui voluptatum cupiditate molestias maiores deleniti ipsam sequi, accusantium consequatur itaque. Beatae suscipit magni ad cum maiores et illo id non minima debitis aliquam expedita incidunt deserunt molestiae explicabo, accusantium rerum similique, excepturi delectus reprehenderit aspernatur dolorum cumque! Maiores exercitationem tempore quidem ipsam tenetur harum quod recusandae facere sapiente! Nostrum, at illum amet assumenda quam, laudantium qui totam saepe, molestias soluta incidunt corporis!',
+    distance: '2.km',
+    duree: '1hour',
+    ville: 'Paris',
+    coordonnee: [
+      2.3544,
+      2.3544,
+    ],
+    images: [
+      'https://images.unsplash.com/photo-1526772662000-3f88f10405ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80',
+    ],
+  },
+
+  {
+    id: 3,
+    titre: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas doloremque autem porro provident, nesciunt nisi optio temporibus, molestiae inventore aliquid fugit ab voluptas, ad iure sapiente quae laudantium impedit suscipit architecto assumenda hic nihil exercitationem ipsa! Soluta qui voluptatum cupiditate molestias maiores deleniti ipsam sequi, accusantium consequatur itaque. Beatae suscipit magni ad cum maiores et illo id non minima debitis aliquam expedita incidunt deserunt molestiae explicabo, accusantium rerum similique, excepturi delectus reprehenderit aspernatur dolorum cumque! Maiores exercitationem tempore quidem ipsam tenetur harum quod recusandae facere sapiente! Nostrum, at illum amet assumenda quam, laudantium qui totam saepe, molestias soluta incidunt corporis!',
+    distance: '2.km',
+    duree: '1hour',
+    ville: 'Paris',
+    coordonnee: [
+      2.3544,
+      2.3544,
+    ],
+    images: [
+      'https://images.unsplash.com/photo-1545652985-5edd365b12eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    ],
+  },
+  {
+    id: 65,
+    titre: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas doloremque autem porro provident, nesciunt nisi optio temporibus, molestiae inventore aliquid fugit ab voluptas, ad iure sapiente quae laudantium impedit suscipit architecto assumenda hic nihil exercitationem ipsa! Soluta qui voluptatum cupiditate molestias maiores deleniti ipsam sequi, accusantium consequatur itaque. Beatae suscipit magni ad cum maiores et illo id non minima debitis aliquam expedita incidunt deserunt molestiae explicabo, accusantium rerum similique, excepturi delectus reprehenderit aspernatur dolorum cumque! Maiores exercitationem tempore quidem ipsam tenetur harum quod recusandae facere sapiente! Nostrum, at illum amet assumenda quam, laudantium qui totam saepe, molestias soluta incidunt corporis!',
+    distance: '2.km',
+    duree: '1hour',
+    ville: 'Paris',
+    coordonnee: [
+      2.3544,
+      2.3544,
+    ],
+    images: [
+      'https://images.unsplash.com/photo-1598524589996-78edc8ddba2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2030&q=80',
+    ],
+  },
+  {
+    id: 8,
+    titre: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas doloremque autem porro provident, nesciunt nisi optio temporibus, molestiae inventore aliquid fugit ab voluptas, ad iure sapiente quae laudantium impedit suscipit architecto assumenda hic nihil exercitationem ipsa! Soluta qui voluptatum cupiditate molestias maiores deleniti ipsam sequi, accusantium consequatur itaque. Beatae suscipit magni ad cum maiores et illo id non minima debitis aliquam expedita incidunt deserunt molestiae explicabo, accusantium rerum similique, excepturi delectus reprehenderit aspernatur dolorum cumque! Maiores exercitationem tempore quidem ipsam tenetur harum quod recusandae facere sapiente! Nostrum, at illum amet assumenda quam, laudantium qui totam saepe, molestias soluta incidunt corporis!',
+    distance: '2.km',
+    duree: '1hour',
+    ville: 'Paris',
+    coordonnee: [
+      2.3544,
+      2.3544,
+    ],
+    images: [
+      'https://images.unsplash.com/photo-1598524589996-78edc8ddba2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2030&q=80',
+    ],
+  },
+
+];
+
+export default data;
