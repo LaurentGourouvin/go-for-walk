462217c73dc408620a9f110508bfaaf43f51a7be
LaurentGourouvin <gourouvin.laurent@gmail.com>
2022-06-16 07:43:16 +0200

    création et modification du composant TrekDetails

--- client/src/Components/TrekDetails/TrekDetails.js
+++ client/src/Components/TrekDetails/TrekDetails.js
@@ -1,40 +1,11 @@
-import { useEffect } from 'react';
 import './TrekDetails.scss';
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
-// import XYZ from 'ol/source/XYZ';
 
 function TrekDetails() {
-  useEffect(() => {
-    const map = new Map({
-      target: 'map',
-      //   layers: [
-      //     new TileLayer({
-      //       source: new XYZ({
-      //         url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-      //       }),
-      //     }),
-      //   ],
-      view: new View({
-        center: [0, 0],
-        zoom: 5,
-      }),
-      layers: [
-        new TileLayer({
-          source: new OSM(),
-        }),
-      ],
-    });
-
-    console.log(map);
-  }, []);
   return (
+
     <div className="TrekDetails">
       <div className="main">
-        <p>MAP OPEN LAYER</p>
-        <div id="map" className="TrekDetails-map" />
+        <p>La randonnée</p>
       </div>
     </div>
 
