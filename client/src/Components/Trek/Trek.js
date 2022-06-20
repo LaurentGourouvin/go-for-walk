0f73fa4e59bc2e1bd2b9f9e64ed0494afaefa8fe
LaurentGourouvin <gourouvin.laurent@gmail.com>
2022-06-20 23:47:47 +0200

    Mise à jour du CSS et préparation pour la fonctionnalité de suppresion d'une randonnée

--- client/src/Components/Trek/Trek.js
+++ client/src/Components/Trek/Trek.js
@@ -5,16 +5,25 @@ import { Link } from 'react-router-dom';
 function Trek({ data }) {
   console.log('Data dans TREK composant:', data);
   return (
-    <div className="Trek">
+    <div className="Trek hover:shadow-lg">
+      <header>{data.title}</header>
+      <hr className="Trek-hr" />
       <figure className="imgTrek">
         {data.pictures ? <img src={data.pictures[0]} alt="Illustration Trek" /> : <img src="" alt="Illustration Trek" /> }
         {/* <img src={data.pictures[0]} alt="Illustration Trek" /> */}
       </figure>
-      <header>{data.title}</header>
+
       <main>{data.description}</main>
       <Link to={`/trek/${data.id}`}>
-        <footer>Bouton pour voir la Trek en détail</footer>
+        <footer className="bg-green-900 text-white active:bg-green-900 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+          Voir le détail de la randonée
+        </footer>
       </Link>
+      <button
+        className="Trek-button bg-red-500 text-white active:bg-red-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+      >Supprimer la randonnée
+      </button>
     </div>
   );
 }
