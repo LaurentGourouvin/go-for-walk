1248655ca34e146cbb5fc8f4162bfa4532159c80
LaurentGourouvin <gourouvin.laurent@gmail.com>
2022-06-20 18:25:34 +0200

    modification de la ligne 21 pour la gestion du token

--- client/src/axios/request.js
+++ client/src/axios/request.js
@@ -2,8 +2,14 @@ import axiosInstance from './config';
 
 const api = {
 
-  // authentification API
-
+  // AUTHENTIFICATION
+  // ======================
+  /**
+   *
+   * @param {*} email email de l'utilisateur permettant la connexion à son espace privé
+   * @param {*} password mot de passe l'utilisateur
+   * @returns me retourne un Token contenant les informations de mon utilisateurs
+   */
   async login(email, password) {
     let login = null;
     try {
@@ -12,7 +18,7 @@ const api = {
         password: password,
       });
       if (login.data) {
-        axiosInstance.defaults.headers.common.Authorization = login.data;
+        axiosInstance.defaults.headers.common.Authorization = `bearer ${login.data.access_token}`;
       }
 
       return login;
@@ -23,6 +29,14 @@ const api = {
     return login;
   },
 
+  /**
+   *
+   * @param {*} firstname firstname de l'utilisateur à créer
+   * @param {*} name prénom de l'utilisateur à créer
+   * @param {*} email email de l'utilisateur à créer
+   * @param {*} password password de l'utilisateur à créer
+   * @returns me retourne les informations de l'utilisateur crée
+   */
   async register(firstname, name, email, password) {
     let register = null;
     try {
@@ -37,6 +51,36 @@ const api = {
     }
 
     return register;
+  },
+
+  // REQUETES SUR LES TREKS
+  /**
+   *
+   * @returns Retourne la liste de toutes les randonnées stockées dans la base de données
+   */
+  async getAllTreks() {
+    let treks = null;
+    try {
+      treks = await axiosInstance.get('/treks');
+    } catch (error) {
+      console.error(error);
+    }
+    return treks;
+  },
+
+  /**
+   *
+   * @param {*} cityName le nom de la ville qui va nous permettre d'effectuer la recherche dans la base de données
+   * @returns Retourne la liste de toutes les randonées stockées dans la base de donnée concernant la ville
+   */
+  async getTreksByCity(cityName) {
+    let treksByCity = null;
+    try {
+      treksByCity = await axiosInstance.get(`/treks/${cityName}`);
+    } catch (error) {
+      console.error(error);
+    }
+    return treksByCity;
   },
 
 };
