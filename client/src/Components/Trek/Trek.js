3b14f28cb8a01a10efda677bcfbf01bfa524d660
LaurentGourouvin <gourouvin.laurent@gmail.com>
2022-06-21 01:52:51 +0200

    essaies de CSS + recherche d'un bug lié au Token. La route api/treks demande un Token alors qu'elle n'est pas censée le faire.

--- client/src/Components/Trek/Trek.js
+++ client/src/Components/Trek/Trek.js
@@ -27,13 +27,15 @@ function Trek({ data, token, setTreksByUserId }) {
       </figure>
 
       <main>{data.description}</main>
-      <Link to={`/trek/${data.id}`}>
-        <footer className="bg-green-900 text-white active:bg-green-900 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
-          Voir le détail de la randonée
-        </footer>
-      </Link>
 
-      {
+      <div className="Trek-buttons-group">
+        <Link to={`/trek/${data.id}`}>
+          <footer className="bg-green-900 text-white active:bg-green-900 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150">
+            Voir le détail de la randonée
+          </footer>
+        </Link>
+
+        {
       userId === data.user_id && location.pathname === '/MyTreks'
         ? (
           <button
@@ -75,7 +77,7 @@ function Trek({ data, token, setTreksByUserId }) {
         )
         : ''
     }
-
+      </div>
     </div>
   );
 }
