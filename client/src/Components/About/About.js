92d502adde357a22dc87de34f4141dda36cc5617
Dorian.A <Dorian.albiges@gmail.com>
2022-06-24 16:59:00 +0200

    Ajout composant About

---
+++ client/src/Components/About/About.js
@@ -0,0 +1,65 @@
+import './About.scss';
+
+function About() {
+  return (
+    <div className="About">
+      <h1 className="About-title">Qui sommes-nous ?</h1>
+      <div className="About-cardContainer">
+        <div className="About-card">
+          <img className="About-img" src="/" alt="" />
+          <div className="About-spanContainer">
+            <span className="About-cardName">Léo Wolff</span>
+            <span className="About-role-back"> Lead Dev Back</span>
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
+            </svg>
+          </div>
+          <p className="About-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu efficitur lorem. Phasellus facilisis mauris ut scelerisque venenatis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris non magna ultrices, convallis massa a, pulvinar eros. Nam nunc libero, tincidunt at dictum vel, rutrum eu felis. Cras vehicula, libero eu condimentum lacinia, risus massa eleifend dui, id ultrices orci sem eget mauris. Ut sapien enim, efficitur et viverra faucibus, dictum eget elit. Nulla consequat luctus facilisis. Ut pretium quis sapien eget egestas. Ut pulvinar egestas est a imperdiet. Donec id nisl id urna facilisis molestie. Duis aliquam sollicitudin varius.
+          </p>
+        </div>
+        <div className="About-card">
+          <img className="About-img" src="/" alt="" />
+          <div className="About-spanContainer">
+            <span className="About-cardName">Franck Bisschop</span>
+            <span className="About-role-back">Git Master</span>
+            <svg xmlns="http://www.w3.org/2000/svg" className="About-icon h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
+            </svg>
+          </div>
+          <p className="About-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu efficitur lorem. Phasellus facilisis mauris ut scelerisque venenatis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris non magna ultrices, convallis massa a, pulvinar eros. Nam nunc libero, tincidunt at dictum vel, rutrum eu felis. Cras vehicula, libero eu condimentum lacinia, risus massa eleifend dui, id ultrices orci sem eget mauris. Ut sapien enim, efficitur et viverra faucibus, dictum eget elit. Nulla consequat luctus facilisis. Ut pretium quis sapien eget egestas. Ut pulvinar egestas est a imperdiet. Donec id nisl id urna facilisis molestie. Duis aliquam sollicitudin varius.
+          </p>
+        </div>
+        <div className="About-card">
+          <img className="About-img" src="/" alt="" />
+          <div className="About-spanContainer">
+            <span className="About-cardName">Laurent Gourouvain</span>
+            <span className="About-role-front">Product Owner</span>
+            <svg xmlns="http://www.w3.org/2000/svg" className="About-icon h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
+            </svg>
+          </div>
+          <p className="About-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu efficitur lorem. Phasellus facilisis mauris ut scelerisque venenatis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris non magna ultrices, convallis massa a, pulvinar eros. Nam nunc libero, tincidunt at dictum vel, rutrum eu felis. Cras vehicula, libero eu condimentum lacinia, risus massa eleifend dui, id ultrices orci sem eget mauris. Ut sapien enim, efficitur et viverra faucibus, dictum eget elit. Nulla consequat luctus facilisis. Ut pretium quis sapien eget egestas. Ut pulvinar egestas est a imperdiet. Donec id nisl id urna facilisis molestie. Duis aliquam sollicitudin varius.
+          </p>
+        </div>
+        <div className="About-card">
+          <img className="About-img" src="/" alt="" />
+          <div className="About-spanContainer">
+            <span className="About-cardName">Dorian Albiges</span>
+            <span className="About-role-front">Lead Dev Front</span>
+            <svg xmlns="http://www.w3.org/2000/svg" className="About-icon h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
+            </svg>
+          </div>
+          <p className="About-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu efficitur lorem. Phasellus facilisis mauris ut scelerisque venenatis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris non magna ultrices, convallis massa a, pulvinar eros. Nam nunc libero, tincidunt at dictum vel, rutrum eu felis. Cras vehicula, libero eu condimentum lacinia, risus massa eleifend dui, id ultrices orci sem eget mauris. Ut sapien enim, efficitur et viverra faucibus, dictum eget elit. Nulla consequat luctus facilisis. Ut pretium quis sapien eget egestas. Ut pulvinar egestas est a imperdiet. Donec id nisl id urna facilisis molestie. Duis aliquam sollicitudin varius.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default About;
