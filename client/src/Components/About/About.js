bd91f0a41840652d81bb76429373b29fb9a8cd9d
Dorian.A <Dorian.albiges@gmail.com>
2022-06-27 14:53:02 +0200

    Ajout / suppresion regle CSS

--- client/src/Components/About/About.js
+++ client/src/Components/About/About.js
@@ -10,7 +10,7 @@ function About() {
           <div className="About-spanContainer">
             <span className="About-cardName">Léo Wolff</span>
             <span className="About-role-back"> Lead Dev Back</span>
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+            <svg xmlns="http://www.w3.org/2000/svg" className="About-icon h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
               <path strokeLinecap="round" strokeLinejoin="round" d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4" />
             </svg>
           </div>
