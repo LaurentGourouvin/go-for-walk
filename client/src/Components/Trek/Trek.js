7a8c62833b095f3170206c1a48fe4ec02a2e8ccc
LaurentGourouvin <gourouvin.laurent@gmail.com>
2022-06-29 10:41:47 +0200

    purge du code

--- client/src/Components/Trek/Trek.js
+++ client/src/Components/Trek/Trek.js
@@ -40,9 +40,6 @@ function Trek({ data, token, setTreksByUserId }) {
             <button
               className="Trek-button bg-stone-500 text-white hover:bg-stone-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
               type="button"
-              onClick={() => {
-
-              }}
             >Modifier la randonnée
             </button>
           </Link>
