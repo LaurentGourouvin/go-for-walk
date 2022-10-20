0fedc1bf8784ad0eeafb16a79d0878802d7c5218
Laurent Gourouvin <gourouvin.laurent@gmail.com>
2022-10-20 16:49:16 +0200

    mise à jour pour deploiement vps

--- client/src/Components/MyTreks/MyTreks.js
+++ client/src/Components/MyTreks/MyTreks.js
@@ -1,19 +1,41 @@
-import './MyTreks.scss';
-import PropTypes from 'prop-types';
-import { useEffect, useState } from 'react';
-import jwtDecode from 'jwt-decode';
-import Trek from '../Trek/Trek';
-import authentification from '../../utils/sessionUser/sessionUser';
-import ImageWarning from './images/warning.png';
-import api from '../../axios/request';
+import "./MyTreks.scss";
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import jwtDecode from "jwt-decode";
+import Trek from "../Trek/Trek";
+import authentification from "../../utils/sessionUser/sessionUser";
+import ImageWarning from "./images/warning.png";
+import api from "../../axios/request";
 
 function MyTreks({ token }) {
   const isLogged = authentification.checkLoggin(token);
   const [treksByUserId, setTreksByUserId] = useState([]);
 
+  useEffect(() => {
+    // Decode du Token
+    decodeToken();
+    if (decodedToken) {
+      // fonction pour récupérer ma liste de treks dans le useEffect de manière asynchrone
+      const getTreks = async () => {
+        try {
+          const treks = await api.getTreksByUserId(decodedToken.userId);
+          if (treks) {
+            setTreksByUserId(treks.data);
+          }
+        } catch (error) {
+          console.error(error);
+        }
+      };
+      getTreks();
+    }
+  }, []);
+
   if (!isLogged) {
     return (
-      <p className="msg-error p-2 bg-amber-50 rounded-md shadow-md"><img src={ImageWarning} alt="logo de warning" />Vous devez être connecté pour consultez vos randonnées</p>
+      <p className="msg-error p-2 bg-amber-50 rounded-md shadow-md">
+        <img src={ImageWarning} alt="logo de warning" />
+        Vous devez être connecté pour consultez vos randonnées
+      </p>
     );
   }
   let decodedToken = null;
@@ -30,31 +52,13 @@ function MyTreks({ token }) {
     return decodedToken;
   };
 
-  useEffect(() => {
-    // Decode du Token
-    decodeToken();
-    if (decodedToken) {
-    // fonction pour récupérer ma liste de treks dans le useEffect de manière asynchrone
-      const getTreks = async () => {
-        try {
-          const treks = await api.getTreksByUserId(decodedToken.userId);
-          if (treks) {
-            setTreksByUserId(treks.data);
-          }
-        } catch (error) {
-          console.error(error);
-        }
-      };
-      getTreks();
-    }
-  }, []);
   return (
     <>
       <h1 className="MyTreks-Title-1">Mes randonnées</h1>
       <div className="MyTreks">
-        {
-        treksByUserId.map((trek) => <Trek key={trek.id} data={trek} token={token} setTreksByUserId={setTreksByUserId} />)
-        }
+        {treksByUserId.map((trek) => (
+          <Trek key={trek.id} data={trek} token={token} setTreksByUserId={setTreksByUserId} />
+        ))}
       </div>
     </>
   );
--- client/src/axios/config.js
+++ client/src/axios/config.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://141.94.207.7:8080/api',
+  baseURL: "http://localhost:5051/api",
 });
 
 export default axiosInstance;
